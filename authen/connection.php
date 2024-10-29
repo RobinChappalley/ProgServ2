9a2df6b237f42637f606925e8fa8515f86851375
RobinChappalley <robinchappalley@gmail.com>
2024-10-29 11:22:59 +0100

    correct css to identify page you're on

--- authen/connection.php
+++ authen/connection.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function createsForm()
 {
     return '<!DOCTYPE html>
@@ -11,8 +10,7 @@ function createsForm()
     <title>Mot de passe</title>
     <link rel="stylesheet" href="style.css" />
   </head>
-  <body>
-  '.addNavBar().'
+  <body>'.addNavBar().'
     <form action="" method="post" class="form-container">
       <div class="form-group">
         <label for="firstname">Prénom :</label>
@@ -55,16 +53,19 @@ function createsForm()
     </form>
   </body>
 </html>
-"';
+';
 }
 
-function addNavBar()  {
-return '<nav class="navbar">
+function addNavBar()
+{
+    return '<nav class="navbar">
     <ul class="nav-list">
       <li><a href="#page1" class="nav-link">Page 1</a></li>
       <li><a href="#page2" class="nav-link">Page 2</a></li>
-      <li><a href="#login" class="nav-link">Se connecter</a></li>
+      <li><a href="#login" class="nav-link" active="true">Se connecter</a></li>
       <li><a href="#signup" class="nav-link">Créer un compte</a></li>
     </ul>
   </nav>';
 }
+
+echo createsForm();
