ae0182c2a0d4ec65187d5245f441c2829b151166
RobinChappalley <robinchappalley@gmail.com>
2024-10-29 10:50:30 +0100

    add navbar and it's css

--- authen/connection.php
+++ authen/connection.php
@@ -12,6 +12,7 @@ function createsForm()
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
+  '.addNavBar().'
     <form action="" method="post" class="form-container">
       <div class="form-group">
         <label for="firstname">Prénom :</label>
@@ -55,4 +56,15 @@ function createsForm()
   </body>
 </html>
 "';
+}
+
+function addNavBar()  {
+return '<nav class="navbar">
+    <ul class="nav-list">
+      <li><a href="#page1" class="nav-link">Page 1</a></li>
+      <li><a href="#page2" class="nav-link">Page 2</a></li>
+      <li><a href="#login" class="nav-link">Se connecter</a></li>
+      <li><a href="#signup" class="nav-link">Créer un compte</a></li>
+    </ul>
+  </nav>';
 }
